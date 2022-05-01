28685cebbd173a5e307cfb3758e8fd5147de61ae
Francisco Ambrosioni <ambrosionifrancisco@gmail.com>
2022-05-01 12:20:07 -0300

    agregamos condicionales

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function iniciar()
 function clickbtnCalcular() 
 {
     let Validar = true;
+    let Validarvariable;
     let txtNombre = document.getElementById("txtNombre");
     let Nombre = txtNombre.value;
 
@@ -19,23 +20,116 @@ function clickbtnCalcular()
     let txtCantDias = document.getElementById("txtCantDias");
     let CantDias = txtCantDias.value;
     
-    /*if (Nombre == '' || Monto<=1000 || CantDias<30)
+    /*---------------------NOMBRE*/
+    if(Nombre == "")
+    {
+        alert(" Vacio");
+    }
+    else
+    {
+        alert(Nombre);
+    }
+    /*------------------------------*/
+
+    /*-----------------------monto*/
+    if(isNaN(Monto))
+    {
+        alert("NaN");
+    }
+    else
+    {
+        alert(Monto);
+        if(Monto>=1000)
+        {
+            alert("monto correcto");
+        }
+        else
+        {
+            alert("El capital debe ser mayor o igual que $1000");
+        }
+    }
+    /*-----------------------CANTDIAS*/
+    if(isNaN(CantDias))
+    {
+    alert("NaN");
+    }
+    else
+    {
+    if(CantDias>=30 )
+    {
+        alert("Dias correctos");
+        Porcentaje();
+    }
+    else
+    {
+        alert("La cantidad de dias debe ser mayor o igual que 30 y menor o igual que 360");
+    }
+    }
+
+    
+}
+
+function Porcentaje()
+{
+    let montoFinal = 0;
+    let porcentaje = 0
+    if(CantDias >=30 && CantDias<=60)
+    {   
+        porcentaje = 40;
+        montoFinal = Monto + Monto *((CantDias/360) * (porcentaje/100));
+    }
+    if(CantDias >=61 && CantDias<=120)
+    {   
+        porcentaje = 45;
+        montoFinal = Monto + Monto *((CantDias/360) * (porcentaje/100));
+    }
+    if(CantDias >=121 && CantDias<=360)
+    {   
+        porcentaje = 50;
+        montoFinal = Monto + Monto *((CantDias/360) * (porcentaje/100));
+    }
+    if(CantDias >360)
+    {   
+        porcentaje = 65;
+        montoFinal = Monto + Monto *((CantDias/360) * (porcentaje/100));
+    }
+    
+
+
+}
+/*if (Nombre == '' || Monto<=1000 || CantDias<30)
     {
         alert("Campos Vacios o Incorrectos");
 
-    }*/
-    if (Monto <=1000)
-    {
-        alert("El monto debe ser mayor o igual que $1000");
-        Validar=false;
+        if(Monto === 'number')
+        {
+        debugger;
+            if(Monto >=1000)
+        {
+            alert("El monto es: $" + Monto);
+        }
+            else
+        {
+            Validar=false;
+            alert("El monto debe ser mayor o igual que $1000");
+        }
+        }
+        else
+        {
+            Validar= false
+            alert("El dato ingresado debe ser un numero");
+        }
         
-    }
+        
+    
+    
     if (CantDias<30)
     {
         alert("La cantidad de dias debe ser igual o superior a 30");
         Validar=false;
     }
-    if (Validar)
+    }
+    /*if (Validar)
     {
         alert("Los datos Ingresados son Correctos");
     }
@@ -43,5 +137,4 @@ function clickbtnCalcular()
     {
         Validar=false;
         
-    }
-}
+    }*/
